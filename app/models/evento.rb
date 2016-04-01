573e0d79043db12260c94178203123642f2393d9
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-04-01 05:38:47 -0500

    Actualiza

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -5,7 +5,7 @@ class Evento < ActiveRecord::Base
   belongs_to :caso, class_name: "Sivel2Gen::Caso", 
     foreign_key: "caso_id"
 
-  has_many :acto, 
+  has_many :actoevento, 
     class_name: "Sivel2Gen::Acto",  
     foreign_key: "id_evento", validate:true, dependent: :destroy
   accepts_nested_attributes_for :acto,
