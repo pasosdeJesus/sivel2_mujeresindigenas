144825135f8ae07261308ee34653806c16684db8
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-04-05 16:43:26 -0500

    adJ 5.8

--- bin/u.sh
+++ bin/u.sh
@@ -4,7 +4,11 @@ if (test "${SECRET_KEY_BASE}" = "") then {
 	echo "Definir variable de ambiente SECRET_KEY_BASE"
 	exit 1;
 } fi;
-sudo su vJtamara -c "cd /var/www/htdocs/sivel2_mujeresindigenas; rm -rf public/assets/*; rake assets:precompile; echo \"Iniciando unicorn...\"; RACK_MULTIPART_LIMIT=2048 SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sivel2_mujeresindigenas/config/unicorn.conf.minimal.rb  -E production -D"
+doas=`which doas 2> /dev/null`
+if (test "$?" != "0") then {
+	doas="sudo"
+} fi;
+$doas su vJtamara -c "cd /var/www/htdocs/sivel2_mujeresindigenas; rm -rf public/assets/*; rake assets:precompile; echo \"Iniciando unicorn...\"; RACK_MULTIPART_LIMIT=2048 SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sivel2_mujeresindigenas/config/unicorn.conf.minimal.rb  -E production -D"
 
 
   
