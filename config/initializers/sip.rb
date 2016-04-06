748b4a7e9067cb88a2c17c297c68c8f2811be617
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-04-06 16:19:26 -0500

    rutas

--- config/initializers/sip.rb
+++ config/initializers/sip.rb
@@ -1,8 +1,8 @@
 require 'sivel2_gen/version'
 
 Sip.setup do |config|
-      config.ruta_anexos = "/var/www/resbase/anexos-mujeresindigenas"
-      config.ruta_volcados = "/var/www/resbase/sivel2_mujeresindigenas/"
+      config.ruta_anexos = "/var/www/resbase/sivel2_mujeresindigenas/anexos/"
+      config.ruta_volcados = "/var/www/resbase/sivel2_mujeresindigenas/bd/"
       # En heroku los anexos son super-temporales
       if !ENV["HEROKU_POSTGRESQL_GREEN_URL"].nil?
         config.ruta_anexos = "#{Rails.root}/tmp/"
