7d3a14dec21036ed3b0c7d8442c936bd07e78db5
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-04-20 03:27:11 -0500

    roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,18 @@
 
 class Ability < Sivel2Sjr::Ability
 
+  ROLADMIN  = 1
+  ROLCOOR   = 4
+  ROLANALI  = 5
+
+  ROLES = [
+    ["Administrador", ROLADMIN], 
+    [nil, nil], 
+    [nil, nil], 
+    ["Administrador oficina", ROLCOOR], 
+    ["Analista", ROLANALI], 
+  ]
+
   @@tablasbasicas = Sip::Ability::BASICAS_PROPIAS + 
     Sivel2Gen::Ability::BASICAS_PROPIAS + 
     Sivel2Sjr::Ability::BASICAS_PROPIAS + 
@@ -36,12 +48,74 @@ class Ability < Sivel2Sjr::Ability
   ]
 
   def initialize(usuario)
-    super(usuario)
     can :nuevo, ::Evento
     can :contar, Sip::Ubicacion
     can :manage, Sivel2Gen::GruposperController
-  end
 
 
-end
+    can :read, [Sip::Pais, Sip::Departamento, Sip::Municipio, Sip::Clase]
+    if !usuario || usuario.fechadeshabilitacion
+      return
+    end
+    can :contar, Sivel2Gen::Caso
+    can :contar, Sip::Ubicacion
+    can :buscar, Sivel2Gen::Caso
+    can :lista, Sivel2Gen::Caso
+    can :descarga_anexo, Sip::Anexo
+    can :nuevo, Sivel2Sjr::Desplazamiento
+    can :nuevo, Sivel2Sjr::Respuesta
+    can :nuevo, Sip::Ubicacion
+    can :nuevo, Sivel2Gen::Presponsable
+    can :nuevo, Sivel2Gen::Victima
+    if !usuario.nil? && !usuario.rol.nil? then
+      if usuario.oficina && usuario.oficina.id  == 104
+        can :edit, :casosacin
+      end
+      case usuario.rol 
+      when Ability::ROLANALI
+        can :read, Sivel2Gen::Caso
+        can :new, Sivel2Gen::Caso
+        can [:update, :create, :destroy], Sivel2Gen::Caso, 
+          casosjr: { oficina_id: usuario.oficina_id }
+        can :read, Cor1440Gen::Informe
+        can :read, Cor1440Gen::Actividad
+        can :new, Cor1440Gen::Actividad
+        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
+          oficina: { id: usuario.oficina_id}
+        can :manage, Sivel2Gen::Acto
+        can :manage, Sip::Persona
+      when Ability::ROLCOOR
+        can :read, Sivel2Gen::Caso
+        can :new, Sivel2Gen::Caso
+        can [:update, :create, :destroy, :poneretcomp], Sivel2Gen::Caso, 
+          casosjr: { oficina_id: usuario.oficina_id }
+        can :manage, Cor1440Gen::Informe
+        can :read, Cor1440Gen::Actividad
+        can :new, Cor1440Gen::Actividad
+        can [:update, :create, :destroy], Cor1440Gen::Actividad, 
+          oficina: { id: usuario.oficina_id}
+        can :manage, Sivel2Gen::Acto
+        can :manage, Sip::Persona
+        can :new, Usuario
+        can [:read, :manage], Usuario, oficina: { id: usuario.oficina_id}
+      when Ability::ROLADMIN, Ability::ROLDIR
+        can :edit, :casosacin
+        can :manage, Sivel2Gen::Caso
+        can :manage, Cor1440Gen::Actividad
+        can :manage, Cor1440Gen::Informe
+        can :manage, Sivel2Gen::Acto
+        can :manage, Sip::Persona
+        can :manage, Usuario
+        can :manage, :tablasbasicas
+        @@tablasbasicas.each do |t|
+          c = Ability.tb_clase(t)
+          can :manage, c
+        end
+      end
+    end
+
+  end # initialize
+
+
+end # class
 
