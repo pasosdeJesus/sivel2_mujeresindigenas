a8dd58a4d20bd7daa10c23ae21e60a94e36a5d43
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-04-20 08:52:15 -0500

    Espacio para privado acin en pestana victima

---
+++ app/models/sip/persona.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+require 'sivel2_sjr/concerns/models/persona'
+
+class Sip::Persona < ActiveRecord::Base
+  include Sivel2Sjr::Concerns::Models::Persona
+
+  def vformatonumdoc
+  end
+end
+
