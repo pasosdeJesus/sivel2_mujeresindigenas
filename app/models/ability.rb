f3f99dd67d035a6cbe8efe863e983d199909afd1
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-11-15 05:52:22 -0500

    Cambia ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,39 +27,41 @@ class Ability < Sivel2Sjr::Ability
 	  ['', 'tapoyo']
   ]
 
-  @@tablasbasicas = Sip::Ability::BASICAS_PROPIAS + 
-    Sivel2Gen::Ability::BASICAS_PROPIAS + 
-    Sivel2Sjr::Ability::BASICAS_PROPIAS + 
-    BASICAS_PROPIAS  - [
-      ['Sivel2Gen', 'filiacion'],
-      ['Sivel2Gen', 'iglesia'],
-      ['Sivel2Gen', 'intervalo'],
-      ['Sivel2Gen', 'frontera'],
-      ['Sivel2Gen', 'intervalo'],
-      ['Sivel2Gen', 'organizacion'],
-      ['Sivel2Gen', 'pconsolidado'],
-      ['Sivel2Gen', 'region'],
-      ['Sivel2Gen', 'sectorsocial'],
-      ['Sivel2Gen', 'vinculoestado'],
-      ['Sivel2Sjr', 'acreditacion'],
-      ['Sivel2Sjr', 'aslegal'],
-      ['Sivel2Sjr', 'ayudasjr'],
-      ['Sivel2Sjr', 'ayudaestado'],
-      ['Sivel2Sjr', 'clasifdesp'],
-      ['Sivel2Sjr', 'comosupo'],
-      ['Sivel2Sjr', 'inclusion'],
-      ['Sivel2Sjr', 'modalidadtierra'],
-      ['Sivel2Sjr', 'declaroante'],
-      ['Sivel2Sjr', 'derecho'],
-      ['Sivel2Sjr', 'motivosjr'],
-      ['Sivel2Sjr', 'personadesea'],
-      ['Sivel2Sjr', 'progestado'],
-      ['Sivel2Sjr', 'proteccion'],
-      ['Sivel2Sjr', 'statusmigratorio'],
-      ['Sivel2Sjr', 'tipodesp']
-  ]
+  def tablasbasicas 
+    Sip::Ability::BASICAS_PROPIAS + 
+      Sivel2Gen::Ability::BASICAS_PROPIAS + 
+      Sivel2Sjr::Ability::BASICAS_PROPIAS + 
+      BASICAS_PROPIAS  - [
+        ['Sivel2Gen', 'filiacion'],
+        ['Sivel2Gen', 'iglesia'],
+        ['Sivel2Gen', 'intervalo'],
+        ['Sivel2Gen', 'frontera'],
+        ['Sivel2Gen', 'intervalo'],
+        ['Sivel2Gen', 'organizacion'],
+        ['Sivel2Gen', 'pconsolidado'],
+        ['Sivel2Gen', 'region'],
+        ['Sivel2Gen', 'sectorsocial'],
+        ['Sivel2Gen', 'vinculoestado'],
+        ['Sivel2Sjr', 'acreditacion'],
+        ['Sivel2Sjr', 'aslegal'],
+        ['Sivel2Sjr', 'ayudasjr'],
+        ['Sivel2Sjr', 'ayudaestado'],
+        ['Sivel2Sjr', 'clasifdesp'],
+        ['Sivel2Sjr', 'comosupo'],
+        ['Sivel2Sjr', 'inclusion'],
+        ['Sivel2Sjr', 'modalidadtierra'],
+        ['Sivel2Sjr', 'declaroante'],
+        ['Sivel2Sjr', 'derecho'],
+        ['Sivel2Sjr', 'motivosjr'],
+        ['Sivel2Sjr', 'personadesea'],
+        ['Sivel2Sjr', 'progestado'],
+        ['Sivel2Sjr', 'proteccion'],
+        ['Sivel2Sjr', 'statusmigratorio'],
+        ['Sivel2Sjr', 'tipodesp']
+      ]
+  end
 
-  def initialize(usuario)
+  def initialize(usuario = nil)
     can :nuevo, ::Evento
     can :contar, Sip::Ubicacion
     can :manage, Sivel2Gen::GruposperController
@@ -126,7 +128,7 @@ class Ability < Sivel2Sjr::Ability
         can :manage, Sip::Persona
         can :manage, Usuario
         can :manage, :tablasbasicas
-        @@tablasbasicas.each do |t|
+        tablasbasicas.each do |t|
           c = Ability.tb_clase(t)
           can :manage, c
         end
@@ -134,7 +136,6 @@ class Ability < Sivel2Sjr::Ability
     end
 
   end # initialize
-
 
 end # class
 
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,8 @@ Rails.application.routes.draw do
     resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
  
     namespace :admin do
-      ::Ability.tablasbasicas.each do |t|
+      ab = ::Ability.new
+      ab.tablasbasicas.each do |t|
         if (t[0] == "") 
           c = t[1].pluralize
           resources c.to_sym, 
