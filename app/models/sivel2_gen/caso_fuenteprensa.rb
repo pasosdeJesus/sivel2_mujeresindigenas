83a1392174880bdcbed1a820b6e72bad8148bbe7
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-01-11 05:54:00 -0500

    Fecha de fuente puede ser anterior a la de registro

---
+++ app/models/sivel2_gen/caso_fuenteprensa.rb
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+
+require 'sivel2_gen/concerns/models/caso_fuenteprensa'
+
+module Sivel2Gen
+  class CasoFuenteprensa < ActiveRecord::Base
+    include Sivel2Gen::Concerns::Models::CasoFuenteprensa
+
+    protected
+
+    def saltar_fuente_tras_caso?
+      true
+    end
+
+  end
+end
