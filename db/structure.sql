ff2642aa348d9605ab6a46b3b2e6a69f6d0f0610
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-04-30 13:53:20 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -385,7 +385,8 @@ CREATE TABLE public.sivel2_gen_caso (
     bienes text,
     id_intervalo integer DEFAULT 5,
     created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    updated_at timestamp without time zone,
+    ubicacion_id integer
 );
 
 
@@ -9036,6 +9037,14 @@ ALTER TABLE ONLY public.mr519_gen_valorcampo
 
 
 --
+-- Name: sivel2_gen_caso fk_rails_850036942a; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sivel2_gen_caso
+    ADD CONSTRAINT fk_rails_850036942a FOREIGN KEY (ubicacion_id) REFERENCES public.sip_ubicacion(id);
+
+
+--
 -- Name: sivel2_sjr_ayudaestado_derecho fk_rails_863c4c40a3; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -10341,6 +10350,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190418014012'),
 ('20190418123920'),
 ('20190418142712'),
-('20190426125052');
+('20190426125052'),
+('20190430112229');
 
 
