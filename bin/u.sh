258e78fec3b2a337c0f3f210f528a58fe0b0ac68
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-03-28 18:08:10 -0500

    produccion

--- bin/u.sh
+++ bin/u.sh
@@ -1,17 +1,58 @@
 #!/bin/sh
-# Inicia produccion
+# Arranca con unicorn --suponiendo que ya se ejecutaron otras labores
+#   necesarias para ejecutar como instalar gemas, generar recursos, 
+#   actualizar índices, etc.  Ver bin/corre
+
+if (test "${DIRAP}" = "") then {
+  echo "Definir directorio de la aplicación en DIRAP"
+  exit 1;
+} fi;
+
+if (test -f "${DIRAP}/.env") then {
+  . $DIRAP/.env
+} fi;
+
 if (test "${SECRET_KEY_BASE}" = "") then {
-	echo "Definir variable de ambiente SECRET_KEY_BASE"
-	exit 1;
+  echo "Definir variable de ambiente SECRET_KEY_BASE"
+  exit 1;
 } fi;
-if (test "$USUARIO_AP" = "") then {
-	echo "Falta USUARIO_AP"
-	exit 1;
+
+if (test "${USUARIO_AP}" = "") then {
+  echo "Definir usuario con el que se ejecuta en USUARIO_AP"
+  exit 1;
 } fi;
+
+defuroot=""
+if (test "${RAILS_RELATIVE_URL_ROOT}" != "") then {
+  defuroot="RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}"
+} fi;
+
 DOAS=`which doas 2> /dev/null`
 if (test "$?" != "0") then {
-	DOAS="sudo"
+  DOAS="sudo"
 } fi;
-$DOAS su $USUARIO_AP -c "cd /var/www/htdocs/sivel2_mujeresindigenas; /bin/rm -rf public/assets/* public/mujeresindigenas/sivel2/assets/* 2> /dev/null"
-$DOAS su $USUARIO_AP -c "cd /var/www/htdocs/sivel2_mujeresindigenas; bin/rails assets:precompile RAILS_ENV=production;"
-$DOAS su $USUARIO_AP -c "cd /var/www/htdocs/sivel2_mujeresindigenas; echo \"Iniciando unicorn...\"; RACK_MULTIPART_LIMIT=2048 SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec unicorn_rails -c ../sivel2_mujeresindigenas/config/unicorn.conf.minimal.rb  -E production -D"
+
+$DOAS su - ${USUARIO_AP} -c "cd $DIRAP; 
+  echo \"== Iniciando unicorn... ==\"; 
+  ${defuroot} BD_CLAVE='${BD_CLAVE}' \
+    BD_PRO=\"${BD_PRO}\" \
+    BD_USUARIO=\"${BD_USUARIO}\" \
+    CONFIG_HOSTS=\"${CONFIG_HOSTS}\" \
+    DIRAP=\"${DIRAP}\" \
+    HEB412_RUTA=\"${HEB412_RUTA}\" \
+    PUERTOUNICORN=\"${PUERTOUNICORN}\" \
+    RAILS_ENV=production \
+    RUTA_RELATIVA=\"${RUTA_RELATIVA}\" \
+    SECRET_KEY_BASE=\"${SECRET_KEY_BASE}\" \
+    SIP_FORMATO_FECHA=\"${SIP_FORMATO_FECHA}\" \
+    SIP_RUTA_ANEXOS=\"${SIP_RUTA_ANEXOS}\" \
+    SIP_RUTA_VOLCADOS=\"${SIP_RUTA_VOLCADOS}\" \
+    SIP_TITULO=\"${SIP_TITULO}\" \
+    SIVEL2_CONSWEB_MAX=\"${SIVEL2_CONSWEB_MAX}\" \
+    SIVEL2_CONSWEB_EPILOGO=\"${SIVEL2_CONSWEB_EPILOGO}\" \
+    SIVEL2_CONSWEB_PIE=\"${SIVEL2_CONSWEB_PIE}\" \
+    SIVEL2_CONSWEB_PUBLICA=\"${SIVEL2_CONSWEB_PUBLICA}\" \
+    SIVEL2_MAPAOSM_DIASATRAS=\"${SIVEL2_MAPAOSM_DIASATRAS}\" \
+    bundle exec /usr/local/bin/unicorn_rails \
+      -c ${DIRAP}/config/unicorn.conf.minimal.rb  -E production -D"
+
--- config/unicorn.conf.minimal.rb
+++ config/unicorn.conf.minimal.rb
@@ -6,11 +6,26 @@
 # See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
 # a more verbose configuration using more features.
 
-listen 3010 # by default Unicorn listens on port 8080
-APP_PATH = "/var/www/htdocs/sivel2_mujeresindigenas"
+if ENV['DIRAP'].nil? 
+  puts "Establezca en DIRAP directorio de aplicacion"
+  return
+end
+if ENV['PUERTOUNICORN'].nil? 
+  puts "Establezca en PUERTOUNICORN el puerto en el que unicorn debe escuchar"
+  return
+end
+procesos=2
+if !ENV['PROCESOSUNICORN'].nil? 
+  procesos=ENV['PROCESOSUNICORN'].to_i
+end
+
+
+
+listen ENV['PUERTOUNICORN'] # by default Unicorn listens on port 8080
+APP_PATH = ENV['DIRAP'] #"/var/www/htdocs/sivel2"
 working_directory APP_PATH
-worker_processes 3 # this should be >= nr_cpus
+worker_processes procesos # this should be >= nr_cpus
 pid APP_PATH + "/tmp/pids/unicorn.pid"
-stderr_path APP_PATH + "/log/unicorn.stderr.log"
-stdout_path APP_PATH + "/log/unicorn.stderr.log"
+stderr_path APP_PATH + "/log/unicorn.log"
+stdout_path APP_PATH + "/log/unicorn.log"
 timeout 3600
