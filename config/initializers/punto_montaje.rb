8a2e036cae8f4872cbab0109ca8e668357d094c0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-04-09 04:04:57 -0500

    Mejora en produccion

--- config/initializers/punto_montaje.rb
+++ config/initializers/punto_montaje.rb
@@ -1,4 +1,4 @@
 Sivel2Mujeresindigenas::Application.config.relative_url_root = ENV.fetch(
-  'RUTA_RELATIVA', '/mujeresindigenas/sivel2/')
+  'RUTA_RELATIVA', '/mujeresindigenas/sivel2')
 Sivel2Mujeresindigenas::Application.config.assets.prefix = ENV.fetch(
-  'RUTA_RELATIVA', '/mujeresindigenas/sivel2/') + 'assets'
+  'RUTA_RELATIVA', '/mujeresindigenas/sivel2') + '/assets'
