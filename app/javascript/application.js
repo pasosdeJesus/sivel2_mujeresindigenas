353f0fada8dc02530e8a6f26c92f25ba1e25d943
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-29 11:59:32 -0500

    Actualiza

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -23,10 +23,8 @@ import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 var L = require('leaflet');
 var mc= require('leaflet.markercluster');
 
-document.addEventListener("DOMContentLoaded", function() {
-
-
-});
+import {AutocompletaAjaxExpreg} from '@pasosdejesus/autocompleta_ajax'
+window.AutocompletaAjaxExpreg = AutocompletaAjaxExpreg
 
 let esperarRecursosSprocketsYDocumento = function (resolver) {
   if (typeof window.puntomontaje == 'undefined') {
@@ -48,8 +46,52 @@ let promesaRecursosSprocketsYDocumento = new Promise((resolver, rechazar) => {
 promesaRecursosSprocketsYDocumento.then((mensaje) => {
   console.log(mensaje)
   var root = window;
+
+  root.campo_fecha_ref_edad = 'caso_casosjr_attributes_fecharec'
   sip_prepara_eventos_comunes(root);
-  // Agregar más inicializaciones a continuación
+  heb412_gen_prepara_eventos_comunes(root);
+  sivel2_gen_prepara_eventos_comunes(root,'antecedentes/causas');
+  sivel2_sjr_prepara_eventos_comunes(root);
+  sivel2_sjr_prepara_eventos_unicos(root);
+
+  $('#caso_casosjr_attributes_fecharec').datepicker({
+    format: 'yyyy-mm-dd',
+    autoclose: true,
+    todayHighlight: true,
+    language: 'es'
+  }).on('changeDate', function (ev) {
+    //  Cambiar edades
+    $('[id^=caso_victima_attributes][id$=persona_attributes_anionac]').change()
+  }
+  )
+
+  $(document).on('change', 'select[id$=_departamentores_id]', function(e) {
+    llena_municipio($(this), root);
+  })
+
+  $('.chosen-select').chosen({
+    allow_single_deselect: true,
+    no_results_text: 'No hay resultados',
+    placeholder_text_multiple: 'Elije una o varias categorias de violencia',
+    width: '100%'
+  })
+
+  $(document).on('cocoon:after-insert', function (e) {
+    $('.chosen-select').chosen({
+      allow_single_deselect: true,
+      no_results_text: 'No hay resultados',
+      placeholder_text_multiple: 'Elije una o varias categorias de violencia',
+      width: '100%'
+    })
+  })
+
+  $(document).on('change', '#mostrarprivados', function(e) {
+    if (this.checked) {
+      $('.privado').show()
+    } else {
+      $('.privado').hide()
+    }
+  })
 })
 
 
